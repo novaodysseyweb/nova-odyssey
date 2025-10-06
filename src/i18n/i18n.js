40875ecdffe1367bffd10a1b0db2c7e287283897
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2025-10-06 20:17:31 +0400

    Automatic language detect

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,18 +1,27 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
 import en from "./en.json";
 import es from "./es.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    es: { translation: es },
-  },
-  lng: "es", // default
-  fallbackLng: "es",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)       // ⬅️ Add this line
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    fallbackLng: "es",          // If no match found, default to Spanish
+    detection: {
+      // How language will be detected
+      order: ["navigator", "htmlTag", "cookie", "localStorage", "path", "subdomain"],
+      caches: ["localStorage", "cookie"], // store user choice
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
