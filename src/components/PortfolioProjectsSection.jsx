54e5d6dedd0c75caf385ef623fcf382d1f38c010
JavierLP99 <mjavlagunes@gmail.com>
2025-10-20 00:13:00 +0000

    Portfolio page modified with modals

--- src/components/PortfolioProjectsSection.jsx
+++ src/components/PortfolioProjectsSection.jsx
@@ -32,11 +32,10 @@ function PortfolioProjectsSection() {
             key={idx}
             className='d-flex justify-content-center align-items-center my-5 col-11 col-sm-10 col-md-6'
           >
-
             <div className='col'>
               <div className='card col-10 col-sm-9 col-lg-12'>
-                <div className='border-bottom'>
-                  <a href={project.link} className='ratio ratio-4x3' target='blank'>
+                <div>
+                  <a href={project.link} className='ratio ratio-16x9' target='blank'>
                     <img
                       src={project.heroImage}
                       className='card-img object-fit-cover'
@@ -44,32 +43,32 @@ function PortfolioProjectsSection() {
                     />
                   </a>
                 </div>
-                <div className='ratio ratio-21x9'>
-                  <div className='card-body d-flex flex-column pb-0'>
-                    <div className='overflow-auto h-75'>
-                      <h5 className='card-title'>{project.name}</h5>
-                      <p className='card-text text-justify'>
-                        {project.client}
-                      </p>
-                      <p className='card-text text-justify'>
-                        {project.details}
-                      </p>
-                    </div>
-                    <div className='d-flex'>
-                      <Button
-                        className='btn-primary rounded-pill deleteprod'
-                        onClick={() => details(project)}
-                      >
-                        Ver más detalles
-                      </Button>
-                    </div>
+
+                <div className='card-body d-flex flex-column'>
+                  <div className='pb-3'>
+                    <h5 className='card-title text-secondary'>{project.name}</h5>
+                    <h6 className='card-text card-subtile text-justify border border-secondary text-secondary rounded d-inline-block p-2'>
+                      {project.client}
+                    </h6>
+                    <p className='card-text text-justify'>
+                      {project.details}
+                    </p>
+                  </div>
+                  <div className='d-flex'>
+                    <Button
+                      variant='outline-primary'
+                      onClick={() => details(project)}
+                    >
+                      Ver más detalles
+                    </Button>
                   </div>
                 </div>
+
               </div>
               <div className='d-flex'>
                 <a
                   href={project.link}
-                  className='btn btn-outline-dark col'
+                  className='btn btn-secondary col'
                   target='blank'
                 >
                   Ir al sitio
@@ -91,8 +90,13 @@ function PortfolioProjectsSection() {
         </Modal.Header>
         {selectedProject && (
           <Modal.Body className='rounded'>
-            <h5 className='card-title'>{selectedProject.name}</h5>
-            <h2 className='text-center text-success'>Producto eliminado</h2>
+            <h5 className='card-tile'>{selectedProject.name}</h5>
+            <h6 className='card-text card-subtile text-justify border border-secondary text-secondary rounded d-inline-block p-2'>
+              {selectedProject.client}
+            </h6>
+            <p className='card-text text-justify'>
+              {selectedProject.details}
+            </p>
             <div className='text-center mt-4'>
               <Button variant='primary' onClick={handleCloseModal}>
                 Cerrar
